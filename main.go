312b2abf91be9b42888127cae2ee6975f4f69adc
Daniel Tremură <tremuradaniel@gmail.com>
2021-08-15 12:41:48 +0300

    byte slices/type conversion

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	greeting := "Hello there!"
+
+	fmt.Println([]byte(greeting))
 
 }
