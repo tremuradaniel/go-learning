92b308acff585c5db5f35cfd9d61fdefd2ec3abd
Daniel Tremură <tremuradaniel@gmail.com>
2021-08-18 17:31:37 +0300

    declaring structs

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
+type person struct {
+	firstName string
+	lastName  string
+}
+
 func main() {
-	ints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for _, v := range ints {
-		if v%2 == 0 {
-			fmt.Printf("%d is even \n", v)
-		} else {
-			fmt.Printf("%d is odd \n", v)
-		}
-	}
+	alex := person{"Alex", "Tim"}
+	// or
+	alexa := person{firstName: "Alexa", lastName: "Stuart"}
+	fmt.Println(alex)
+	fmt.Println(alexa)
 }
