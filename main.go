32363e8f8a0be57e2711b9ff14c3af5bc7c6c58c
Daniel Tremură <tremuradaniel@gmail.com>
2021-08-18 17:19:14 +0300

    determine odd/even

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-
-	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
+	ints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, v := range ints {
+		if v%2 == 0 {
+			fmt.Printf("%d is even \n", v)
+		} else {
+			fmt.Printf("%d is odd \n", v)
+		}
+	}
 }
