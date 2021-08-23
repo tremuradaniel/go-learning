132c424a1b79e15d4d3c7ce494b51947b3936182
Daniel Tremură <tremuradaniel@gmail.com>
2021-08-23 10:31:01 +0300

    manipulating maps

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red": "#FF0000",
-	}
+
+	// var colors map[string][string]
+
+	colors := make(map[int]string)
+
+	// colors := map[string]string{
+	// 	"red": "#FF0000",
+	// }
+
+	colors[1] = "blue"
+
+	fmt.Println(colors)
+
+	delete(colors, 1)
+
 	fmt.Println(colors)
 }
