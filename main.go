8f8ea551b13ff63a394c8d8b7a405841a206a487
Daniel Tremură <tremuradaniel@gmail.com>
2021-08-23 10:19:58 +0300

    maps

--- main.go
+++ main.go
@@ -2,36 +2,9 @@ package main
 
 import "fmt"
 
-type contractInfo struct {
-	email   string
-	zipCode int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	contractInfo
-}
-
 func main() {
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Patty",
-		contractInfo: contractInfo{
-			email:   "test@test.com",
-			zipCode: 303212,
-		},
+	colors := map[string]string{
+		"red": "#FF0000",
 	}
-
-	jim.updateName("Jimmy")
-	jim.print()
-
-}
-
-func (pointerToPerson *person) updateName(newName string) {
-	(*pointerToPerson).firstName = newName
-}
-
-func (p person) print() {
-	fmt.Printf("%-v", p)
+	fmt.Println(colors)
 }
